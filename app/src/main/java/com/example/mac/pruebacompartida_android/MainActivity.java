03e463d01ba05bf5a2ad01fb3fdc8ebe095c0aad
Braulio Ovalles <braulioovalles7d@outlook.com>
2019-06-02 18:14:26 -0400

    Boton segunadaActividad

--- app/src/main/java/com/example/mac/pruebacompartida_android/MainActivity.java
+++ app/src/main/java/com/example/mac/pruebacompartida_android/MainActivity.java
@@ -1,18 +1,39 @@
 package com.example.mac.pruebacompartida_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     String nombreVendedor = "";
     String apellido="";
+    Button btnIrActivity2_;
+    EditText txtEscribeUnNombre_;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtEscribeUnNombre_ = (EditText) findViewById(R.id.txtEscribeUnNombre);
+        btnIrActivity2_ = (Button) findViewById(R.id.btnIrActivity2);
+
+        btnIrActivity2_.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SegunaActivity.class);
+                intent.putExtra("nombreUsuario", txtEscribeUnNombre_.getText().toString());
+                startActivity(intent);
+
+            }
+        });
+
+
 
         MetodoSaludar();
 
--- app/src/test/java/com/example/mac/pruebacompartida_android/Tests/ExampleUnitTest.java
+++ app/src/test/java/com/example/mac/pruebacompartida_android/Tests/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.mac.pruebacompartida_android;
+package com.example.mac.pruebacompartida_android.Tests;
 
 import org.junit.Test;
 
