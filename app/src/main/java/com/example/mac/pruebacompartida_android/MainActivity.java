25e6cb61217b2eceb51d7e8bda2b5341b85a8e32
Braulio Ovalles <braulioovalles7d@outlook.com>
2019-06-02 00:49:39 -0400

    main Activity

--- app/src/main/java/com/example/mac/pruebacompartida_android/MainActivity.java
+++ app/src/main/java/com/example/mac/pruebacompartida_android/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.mac.pruebacompartida_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String nombreVendedor = "";
+    String apellido="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
     }
+
+
+
+    //saludar
+    public void MetodoSaludar(){
+        Toast.makeText(getApplicationContext(), "Hola mundo", Toast.LENGTH_LONG).show();
+    }
+
 
 }
---
+++ app/src/main/java/com/example/mac/pruebacompartida_android/Vendedor.java
@@ -0,0 +1,6 @@
+package com.example.mac.pruebacompartida_android;
+
+public class Vendedor {
+
+
+}
