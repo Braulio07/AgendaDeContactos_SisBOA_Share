ebeb0d47eeb1063328ec8bb312554c5268d70e27
Braulio Ovalles <braulioovalles7d@outlook.com>
2019-06-02 18:22:58 -0400

    CREACION segunadaActividad

--- app/src/main/java/com/example/mac/pruebacompartida_android/SegunaActivity.java
+++ app/src/main/java/com/example/mac/pruebacompartida_android/SegunaActivity.java
@@ -2,12 +2,19 @@ package com.example.mac.pruebacompartida_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SegunaActivity extends AppCompatActivity {
 
+    TextView txtSegundaActividad_;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seguna);
+
+        String nombreMostrar = getIntent().getStringExtra("nombreUsuario");
+
+        txtSegundaActividad_ = (TextView)findViewById(R.id.txtSegundaActividad);
+        txtSegundaActividad_.setText("Bienvenido: "+nombreMostrar);
     }
 }
